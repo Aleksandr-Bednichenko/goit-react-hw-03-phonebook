fdd679491f373533b9a427f6922df9084383339b
Aleksandr-Bednichenko <a0933093518@gmail.com>
2021-07-20 12:47:53 +0300

    материалі из дз 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import MyForm from './Components/MyForm';
+import Filter from './Components/Filter';
+import ContactsList from './Components/ContactsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  onSubmitForm = ({ name, number }) => {
+    const { contacts } = this.state;
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+    contacts.some(i => i.name === name)
+      ? alert(`${name} is already in contacts`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+    console.log(contact);
+  };
+
+  onFilterInput = e => {
+    const value = e.currentTarget.value;
+    this.setState({ filter: value });
+  };
+
+  onFilterChange = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(i =>
+      i.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  onDeleteContactClick = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(el => el.id !== id),
+    }));
+  };
+
+  render() {
+    const contacts = this.onFilterChange();
+    const { filter } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <MyForm onSubmit={this.onSubmitForm} />
+        <Filter value={filter} onFilter={this.onFilterInput} />
+        <h2>Contacts</h2>
+        <ContactsList
+          contacts={contacts}
+          onDeleteClick={this.onDeleteContactClick}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
